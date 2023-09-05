be676bcb751c9d451c7ad53504ede6b053cdc01a
trindadeee <arthurtrintrincordeiro@gmail.com>
2023-09-05 14:06:32 -0300

    estudando conversoes de tipos de dados no js

---
+++ conversoes.js
@@ -0,0 +1,23 @@
+//conversao implicita: converte um tipo de dado em outro, ou seja converter numero em string e vice versa, por exemplo
+
+const numero = 444;
+//se tentar converter string que contenham letras vai ocorrer o erro NaN 
+const numeroString = Number("444a");
+
+console.log(numero === numeroString);
+
+//pego o numero 444 converteu em string e comparou e o resultado é true
+console.log(numero == numeroString);
+
+//pego o numero, transformou em string e com a operacao(+) ele concatenou 2 strings, resultando em 444444 
+console.log(numero + numeroString);
+
+// conversao explicita: é utilizada 2 funções para a conversão sendo elas a Number() e a String().
+
+console.log(numero + numeroString);
+
+// pode ser feita a conversao na propria variavel fazendo isso const numeroString = NUmber("444");
+
+//const numeroString = "444";
+
+
